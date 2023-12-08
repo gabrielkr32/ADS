329b7768dc049d9548212a837eb2ece3680496ef
Gabriel Kruger <gabrielkr32@gmail.com>
2023-12-07 22:06:03 -0300

    Pseudo

---
+++ Python/Pseudocódigo.py
@@ -0,0 +1,43 @@
+
+
+
+
+nota_um = int(input("Digite o valor da nota um: "))
+print(nota_um)
+
+nota_dois = int(input ("Digite o valor da segunda nota: "))
+print(nota_dois)
+
+soma = (nota_um + nota_dois)
+media = ( soma / 2 )
+print("A media do aluno: ", media)
+
+if media >= 6:
+    print("Aluno aprovado! ")
+else:
+    print("Aluno reprovado")    
+
+
+#escreva (“Digite o valor 1”);
+
+ #    leia valor1;
+
+ #    escreva (“Digite valor 2”);
+
+ #    leia valor2;
+
+ #    soma 
+
+ #valor1 + valor2;
+
+ #   soma 
+
+# soma/2;
+
+ #    escreva(“A media do aluno e:”; media);
+
+ #    se (media >=6)
+
+ #         então escreva(“Aluno Aprovado”);
+
+#          senão escreva(“Aluno Reprovado”);    
